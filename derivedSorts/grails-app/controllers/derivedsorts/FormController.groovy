dde205922771a88cfe1f85140a48e7ec416f8110
Joe Little <jlittle@ee.stanford.edu>
2013-05-29 15:00:31 -0700

    alter sorting for derived fields

--- derivedSorts/grails-app/controllers/derivedsorts/FormController.groovy
+++ derivedSorts/grails-app/controllers/derivedsorts/FormController.groovy
@@ -11,7 +11,32 @@ class FormController {
 		params.offset = offset
 		params.sort = sort ?: 'courseName'
 		params.order = order ?: 'asc'
-        [formInstanceList: ExampleForm.list(params), formInstanceTotal: ExampleForm.count()]
+		def total = ExampleForm.count()
+		def forms
+		if (params.sort == 'courseName')
+		 	forms = ExampleForm.list(params)
+		else if (sort in ['fullName', 'status'])
+			if (sort != 'fullName')  // sort secondarily on fullName
+			{
+				if (order == 'desc') {
+					// sort on secondary criteria first, always ascending on second criteria
+					forms = ExampleForm.list().sort { p1, p2 -> (p1.fullName() <=> p2.fullName())}
+					forms = forms.sort { p1, p2 -> -(p1."$sort"() <=> p2."$sort"())}
+				}
+				else {
+					forms = ExampleForm.list().sort { p1, p2 -> (p1.fullName() <=> p2.fullName())}
+					forms = forms.sort { p1, p2 -> p1."$sort"() <=> p2."$sort"()}
+					
+				}
+			}
+			else 
+			{
+				if (order == 'desc')
+					forms = ExampleForm.list().sort { p1, p2 -> -(p1."$sort"() <=> p2."$sort"())}
+				else
+					forms = ExampleForm.list().sort { p1, p2 -> p1."$sort"() <=> p2."$sort"()}
+			}
+        [formInstanceList: forms, formInstanceTotal: total]
 	}
 	
 }
